d998627dd2aeb1b4b187ce57275484104569013c
Gidy30B <xangida@gmail.com>
2024-12-18 09:12:32 +0300

    made changes to the RootLayout

--- app/_layout.js
+++ app/_layout.js
@@ -2,26 +2,42 @@ import { Stack } from "expo-router";
 import queryClient from "./(services)/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import store from "./(redux)/store";
-import { Provider } from "react-redux";
-import AppWrapper from "./(redux)/AppWrapper";
+import { Provider, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loadUser } from "./(redux)/authSlice";
 import { PaperProvider } from 'react-native-paper'; // Import PaperProvider
 
-export default function RootLayout() {
+function RootLayout() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false, 
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen name="auth/registar" options={{ title: "Register" }} />
+          <Stack.Screen name="auth/login" options={{ title: "Login" }} />
+        </Stack>
+      </PaperProvider>
+    </QueryClientProvider>
+  );
+}
+
+// Wrap the RootLayout component with the Provider from react-redux
+function App() {
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <PaperProvider>
-          <Stack
-            screenOptions={{
-              headerShown: false, // Apply this globally to all screens in this Stack
-            }}
-          >
-            <Stack.Screen name="index" options={{ title: "Home" }} />
-            <Stack.Screen name="auth/registar" options={{ title: "Register" }} /> {/* Corrected route name */}
-            <Stack.Screen name="auth/login" options={{ title: "Login" }} />
-          </Stack>
-        </PaperProvider>
-      </QueryClientProvider>
+      <RootLayout />
     </Provider>
   );
 }
+
+export default App;
