5f3e03c4fe58d589dc4493610f1b07680921b946
Parrot <greggambugua@gmail.com>
2025-01-31 15:55:21 +0300

    Update insurance.js

--- api/controller/insurance.js
+++ api/controller/insurance.js
@@ -76,6 +76,20 @@ exports.updateInsuranceData = async (req, res) => {
   }
 };
 
+// Get insurance data by userId
+exports.getInsuranceByUserId = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const insuranceData = await Insurance.findOne({ userId });
+    if (!insuranceData) {
+      return res.status(404).json({ error: 'Insurance data not found' });
+    }
+    res.status(200).json(insuranceData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 // Delete an insurance provider
 exports.deleteInsuranceProvider = async (req, res) => {
   try {
