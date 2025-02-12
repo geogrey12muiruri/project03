898c06040cdb5547accf35276b554ec813c36ee8
geogrey12muiruri <greggambugua@gmail.com>
2025-02-12 11:27:46 +0300

    added moment

--- api/notificationScheduler.js
+++ api/notificationScheduler.js
@@ -4,21 +4,22 @@ const Appointment = require('./model/appointment.model');
 const User = require('./model/User');
 const { sendPushNotification } = require('./service/notificationService');
 
-// Schedule a job to run every hour
-cron.schedule('0 * * * *', async () => {
+// Schedule a job to run every 15 minutes
+cron.schedule('*/15 * * * *', async () => {
     console.log('Running scheduled job to check for upcoming appointments');
 
     try {
-        const now = moment();
-        const oneHourLater = moment().add(1, 'hour');
+        const now = moment().utc();
+        const oneHourLater = moment().utc().add(1, 'hour');
 
-        // Find appointments that are scheduled within the next hour
+        // Find appointments that are scheduled within the next hour and haven't been notified yet
         const appointments = await Appointment.find({
             date: {
                 $gte: now.toDate(),
                 $lt: oneHourLater.toDate()
             },
-            status: 'confirmed'
+            status: 'confirmed',
+            notificationSent: { $ne: true } // Ensure notifications are only sent once
         });
 
         for (const appointment of appointments) {
@@ -30,6 +31,10 @@ cron.schedule('0 * * * *', async () => {
                     body: `You have an upcoming appointment at ${moment(appointment.date).format('HH:mm')}.`,
                     data: { appointmentId: appointment._id },
                 });
+
+                // Mark the appointment as notified
+                appointment.notificationSent = true;
+                await appointment.save();
             }
         }
     } catch (error) {
